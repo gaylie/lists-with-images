0ba4c4de050d553e18e5d54ebe7a357bfdfa21e1
trashgaylie <dothelicklick@gmail.com>
2026-05-29 19:49:37 +0200

    added about and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { loadEnv } from "vite";
+import sitemap from '@astrojs/sitemap';
+const { BASEURL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
-    outDir: './docs',
-    output: 'static',
-    site: "https://gaylie.github.io/lists-with-images/"
+  outDir: './docs',
+  output: 'static',
+  site: BASEURL,
+  integrations: [sitemap()]
 });
